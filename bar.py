6cd959f24ce5d8dc57c019de5e57ffb1ad66e981
ChristianQC <46302054+ChristianQC@users.noreply.github.com>
2021-04-23 11:26:26 -0500

    Update bar.py

--- bar.py
+++ bar.py
@@ -25,20 +25,25 @@ aud_x = np.arange(0,FRAMES,1)
 x_fft = np.linspace(0,Fs,FRAMES) #
 
 line, = ax.plot(aud_x,np.zeros(FRAMES))
-line_fft, = ax.semilogx(x_fft,np.zeros(FRAMES))
-ax.set_ylim(-10,10)
-ax.set_xlim(1,5000)
+line_fft, = ax.semilogx(x_fft,np.zeros(FRAMES)) #Define la escala para el ejex para la señal en frecuencia
+ax.set_ylim(-10,10) #ejey  
+ax.set_xlim(1,5000) #ejex
 fig.show()
 
 while True:
     data = stream.read(FRAMES)
-    dataInt = struct.unpack(str(FRAMES)+ "h",data)
-    yx = np.asarray(dataInt)
+    dataInt = struct.unpack(str(FRAMES)+ "h",data) #dataInt es la señal del microfono en el tiempo
+    
+    yx = np.asarray(dataInt) #dataInt inicialmente es tupla y se trabajara cono array numpy
+    #Normalizacion de la señal yx a +-1
     ymax = np.max(yx)
     ymin = np.min(yx)
-    yx = 2*(yx - ymin)/(ymax -ymin)-1
+    yx = 2*(yx - ymin)/(ymax -ymin)-1 #yx ya normalizado
+    #Sacamos la transformada y luego la magnitud -> M
     M = abs(fourier.fft(yx))
+    #Deinifimos un eje y dinamico segun la amplitud de la Magnitud ->M
     ax.set_ylim(0,np.max(M)+10)
+    #Graficamos M
     line_fft.set_ydata(M)
     fig.canvas.draw()
     fig.canvas.flush_events()
