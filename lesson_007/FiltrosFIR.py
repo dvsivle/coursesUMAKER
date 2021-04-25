ab6877a4a7f846942ec2422e54ee7981050de78e
NoeVG <noe-vg@outlook.com>
2021-04-24 19:26:18 -0500

    code finish

--- lesson_007/FiltrosFIR.py
+++ lesson_007/FiltrosFIR.py
@@ -26,6 +26,10 @@ import matplotlib.animation as animation
 
 # Library used for scientific computing
 import scipy.fftpack as fourier
+from scipy.fftpack import fftfreq
+from scipy.fftpack import fftshift
+
+
 from scipy import fft, arange
 from scipy import signal
 import scipy
@@ -42,11 +46,10 @@ import struct
 
 globalStream = []
 
-
 rows = 2
 cols = 1
 
-max_axis_X = 1000
+max_length = 1000
 max_axis_Y = 100
 
 # Create an interface to PortAudio
@@ -61,37 +64,40 @@ FRAMES = 1
 FORMAT = pyaudio.paInt16
 # Chanell to get
 CHANNEL = 1
+# Time record seconds
+TIME = 1
 # Record at 44100 samples per second
 #Fs = 44100
 Fs = 44100
 
-"""
-Recording
-"""
-def recording(outDatas):    
-    # Get the stream
-    # Strcture to the stream
-    stream = audioRecord.open(
-        format = FORMAT,
-        channels =  CHANNEL,
-        rate = Fs,
-        input = True,
-        output = True,
-        frames_per_buffer = FRAMES
-        )
+# Structure to the stream
+stream = audioRecord.open(
+    format = FORMAT,
+    channels =  CHANNEL,
+    rate = Fs,
+    input = True,
+    output = True,
+    frames_per_buffer = FRAMES
+    )
+
+
+
+def recording(outDatas):
+    """
+    Recording
+    """
     try:
+        # Get the stream
         while (True):
             # Get bytes
             data = stream.read(FRAMES) 
             # Get time signal data
             outData = struct.unpack( str(FRAMES)+ "h",data)
-            outDatas.append( outData[0] )
-                        
-            #i = input()
-            if len( outDatas ) > max_axis_X:
+            # Get tuple data
+            outDatas.append( outData[0] )                        
+            if len( outDatas ) > max_length:
+                # remove the first element
                 outDatas.pop(0)
-                
-    
     except KeyboardInterrupt:
         # Stop and close the stream 
         stream.stop_stream()
@@ -147,7 +153,6 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     b, a = butter(order, [low, high], btype='band')
     return b, a
 
-
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
@@ -162,127 +167,128 @@ def butter_highpass_filter(data,cutoff,fs, order=5):
     b, a = butter_highpass(cutoff,fs,order=order)
     y = lfilter(b, a, data)
     return y
-def getFrecuecyDomain(data):
-    x = data - np.average(data)  # zero-centering
 
-    n = len(x)
-    k = arange(n)
-    tarr = n / float(Fs)
-    frqarr = k / float(tarr)  # two sides frequency range
+def plotFrecuecyDomain(chart,line,data):
+    '''
+    Plot data in the subplot and set data in the line
+    '''
+    NFFT=1024 #NFFT-point DFT      
+    X = fftshift( fourier.fft(data,NFFT) ) #compute DFT using FFT  
+    
+    #fVals = np.arange(start = -NFFT/2,stop = NFFT/2)/NFFT #DFT Sample points        
+    fVals=np.arange(start = -NFFT/2,stop = NFFT/2)*Fs/NFFT
 
-    frqarr = frqarr[range(n // 2)]  # one side frequency range
+    # Get the limit to axis
+    valuesX = fVals 
+    valuesY = np.abs( X )
 
-    x = fourier.fft(x) / n  # fft computing and normalization
-    x = x[range(n // 2)]
-   
-    return  frqarr, x
-"""
-Plot data
-"""
-def plotData(num,ax1,ax2,hl1,hl2,hl3,hll1,hll2,hll3,data):
+    limitAxisY = max(valuesY) + 10
+    limitAxisX = max(valuesX) + 10
     
-    # plot time domain
-    valuesX = list( range(0,len(data)) )
-    maxAxisY = max(data) + 200
+    # Set the limit to axis
+    chart.set_ylim( 0,limitAxisY )
+    chart.set_xlim( -1*limitAxisX,limitAxisX )
+    #chart.set_xlim( -5,5 )
     
-    ax1.set_ylim( -1*maxAxisY,maxAxisY)
-    hl1.set_data(valuesX , data)
-    hl1.set_color("blue")
-    
-    # plot frecuency
-    
-    data_ = data.copy()
-    dataX, dataY = getFrecuecyDomain(data_)
-    maxAxisY = max(dataY) + 1
-    maxAxisX = max(dataX)
-    ax2.set_xlim(0, maxAxisX )
-    ax2.set_ylim( 0, maxAxisY )
-    
-    hll1.set_data( dataX,abs(dataY))
-    hll1.set_color("blue")
+    # Prepare data to X
+    #valuesX = list( range(0,length) )
+    # Set values Y
+    line.set_data(valuesX , valuesY)
 
+def plotAmplitudeDomain(chart,line,data):
+    '''
+    Plot data in the subplot and set data in the line
+    '''
+    # Get the length data 
+    length = len(data)
+    # Get the limit to axis
+    limitAxisY = max(data) + 10
+    limitAxisX = length + 10
+    
+    # Set the limit to axis
+    chart.set_ylim( -1*limitAxisY,limitAxisY )
+    chart.set_xlim( 0,limitAxisX )
+    # Prepare data to X
+    valuesX = list( range(0,length) )
+    # Set values Y
+    line.set_data(valuesX , data)
+
+
+def plotData(num,charst,lines,data):
+    """
+    Plot data
+    """
     # Sample rate and desired cutoff frequencies (in Hz).    
     fs = Fs
     lowcut = 500.0
     highcut = 1250.0
 
-    #datay = butter_bandpass_filter(data, lowcut, highcut, fs, order=6)
-    #print(datay)
-
     data_ = data.copy()
     #Butterworth low-pass
     lowPass = butter_lowpass_filter(data_, lowcut,fs, order=6)
-
-    valuesX = list( range(0,len(lowPass)) )
-    maxAxisY = max(lowPass) + 200
-    hl2.set_data(valuesX, lowPass)
-    hl2.set_color("orange")
-
-    data_ = lowPass.copy()
-    dataX, dataY = getFrecuecyDomain(data_)
-    maxAxisY = max(dataY) + 1
-    maxAxisX = max(dataX)
-    ax2.set_xlim(0, maxAxisX )
-    ax2.set_ylim( 0, maxAxisY )
-    
-    hll2.set_data( dataX,abs(dataY))
-    hll2.set_color("orange")
-
 
     data_ = data.copy()
     #Butterworth high-pass
     highPass = butter_highpass_filter(data_, lowcut,fs, order=6)
 
-    valuesX = list( range(0,len(highPass)) )
-    maxAxisY = max(highPass) + 200
-    hl3.set_data(valuesX, highPass)
-    hl3.set_color("green")
 
-    data_ = highPass.copy()
-    dataX, dataY = getFrecuecyDomain(data_)
-    maxAxisY = max(dataY) + 1
-    maxAxisX = max(dataX)
-    ax2.set_xlim(0, maxAxisX )
-    ax2.set_ylim( 0, maxAxisY )
-    
-    hll3.set_data( dataX,abs(dataY))
-    hll3.set_color("green")
+    # plot time domain
+    plotAmplitudeDomain(charst[0],lines[0],data)
+    plotAmplitudeDomain(charst[0],lines[1],lowPass)
+    plotAmplitudeDomain(charst[0],lines[2],highPass)
 
-    return hl1,hl2,hl3,hll1,hll2,hll3
+    # plot frecuency    
+    plotFrecuecyDomain(charst[1],lines[3],data)
+    plotFrecuecyDomain(charst[1],lines[4],lowPass)
+    plotFrecuecyDomain(charst[1],lines[5],highPass)
 
-"""
-This function implements the estimation of 
-the heart rate by means of digital filters.
-"""
+    return charst,lines
+
+
 def main():    
+    """
+    This function implements the estimation of 
+    the heart rate by means of digital filters.
+    """
+    lines = []
+    charst = []
+    
     fig, (ax1,ax2) = plt.subplots(2)
+    charst.append(ax1)
+    charst.append(ax2)
+
     fig.suptitle('Filtros FIR')
     
-    ax1.set_title('Time domain')        
-    ax1.set_ylabel('Amplitude')    
-    ax1.set_xlabel('Time')    
+    charst[0].set_title('Time domain')        
+    charst[0].set_ylabel('Amplitude')    
+    charst[0].set_xlabel('Time')    
     
-    ax1.set_ylim(-1*max_axis_Y,max_axis_Y)   
-    ax1.set_xlim(1,max_axis_X)
-
     hl1, = ax1.plot( globalStream, globalStream ,color="blue", label='Stream')
-    hl2, = ax1.plot( globalStream, globalStream)
-    hl3, = ax1.plot( globalStream, globalStream)
-
-    ax2.set_title('Frequency domain')        
-    ax2.set_ylabel('|Y(f)|')    
-    ax2.set_xlabel('Frequency')    
+    lines.append(hl1)
     
-    ax2.set_ylim(-1*max_axis_Y,max_axis_Y)   
-    ax2.set_xlim(1,max_axis_X)
-    hll1, = ax2.plot( globalStream, globalStream,color="blue",label='Stream')
-    hll2, = ax2.plot( globalStream, globalStream,color="orange",label='Butterworth low-pass')
-    hll3, = ax2.plot( globalStream, globalStream,color="green",label='Butterworth high-pass')
+    hl2, = ax1.plot( globalStream, globalStream ,color="green",label='Butterworth low-pass' )
+    lines.append(hl2)
+    
+    hl3, = ax1.plot( globalStream, globalStream,color="red",label='Butterworth high-pass')
+    lines.append(hl3)
 
+    hll1, = ax2.plot( globalStream, globalStream ,color="blue", label='Stream')
+    lines.append(hll1)
+    
+    hll2, = ax2.plot( globalStream, globalStream ,color="green",label='Butterworth low-pass')
+    lines.append(hll2)
+    
+    hll3, = ax2.plot( globalStream, globalStream,color="red",label='Butterworth high-pass')
+    lines.append(hll3)
+
+    charst[1].set_title('Frequency domain')        
+    charst[1].set_ylabel('|Y(f)|')    
+    charst[1].set_xlabel('Frequency')    
+    
     # Animate matplot
     line_ani = animation.FuncAnimation(fig,
                                        plotData,
-                                       fargs=(ax1,ax2,hl1,hl2,hl3,hll1,hll2,hll3,globalStream),
+                                       fargs=(charst,lines,globalStream),
                                        frames=1,
                                        interval=100,
                                        blit=False
@@ -296,8 +302,9 @@ def main():
     plt.show()
     dataCollector.join()
 
-"""
-Function to execute main
-"""
+
 if __name__ == "__main__":
+    """
+    Function to execute main
+    """
     main()
